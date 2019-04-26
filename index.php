ec2c232c4da62ecf75f6cf1e77495bb7d08ef3dd
Hector <32627138+lewdxample@users.noreply.github.com>
2019-04-26 14:00:16 +0700

    Database Added

--- index.php
+++ index.php
@@ -4,56 +4,42 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" media="screen" href="main1.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="icon.png">
+    <script src="https://www.gstatic.com/firebasejs/5.10.1/firebase.js"></script>
+    <script>
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBt7B_guC2BqwSAdTtKvGbRmu57ziqb_RM",
+        authDomain: "tchest-0.firebaseapp.com",
+        databaseURL: "https://tchest-0.firebaseio.com",
+        projectId: "tchest-0",
+        storageBucket: "tchest-0.appspot.com",
+        messagingSenderId: "381630158312"
+    };
+    firebase.initializeApp(config);
+    </script>
     <script src="main.js"></script>
-	<link rel="icon" href="icon.png">
     <title>Treasure Chest</title>
 </head>
 <body>
     <div class="container">
         <img id="gif" src="shake.gif" width="400" height="250" 
-                onclick=" 
+                onclick="winCount();
                 var chestOpen = new Audio('kaching.m4a');
-                var rand = Math.floor(Math.random() * 2) + 1;
-                console.log(rand);
+                // var rand = Math.floor(Math.random() * 2) + 1;
+                // console.log(rand);
                 chestOpen.play();
                 chestOpen.currentTime=0;
-                this.onclick = null;
-                if(rand == 1){
-                    this.src = 'win.gif';
-                    setTimeout(setTextWin, 1200);
-                }else{
-                    this.src = 'gagal.gif';
-                    setTimeout(setTextZonk, 1200);
-                }">
+                this.onclick = null;">
     </div>
 
-    <p id="p">TAP THE CHEST TO OPEN!!!!</p>
+    <span id="editText">TAP THE CHEST TO OPEN!!!!</span>
 
     <br>
     <br>
 
     <!-- <iframe src="https://www.youtube.com/embed/tgbNymZ7vqY" frameborder="0" width="420" height="315">
     </iframe> -->
-
-    <script>
-        var text = document.getElementById('p');
-        console.log("awkawkawkak");
-        console.log(navigator.onLine);
-        navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
-
-        function setTextWin(){
-            text.textContent = 'Selamat anda mendapatkan Grand Prize!!!'
-        }
-
-        function setTextZonk(){
-            vibrate(1200);
-            text.textContent = 'Maaf, Anda belum Beruntung~';
-        }
-
-        function vibrate(ms){
-            navigator.vibrate(ms);
-        }
-    </script>
 
 </body>
 </html>
